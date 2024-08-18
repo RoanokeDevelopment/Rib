6f70dfc7b8f4ab23ff0a1d10a53490e7358a9d5a
Xander Forrest <xander@xanderforrest.com>
2024-08-18 17:41:56 +0100

    - ConfiguredGUI supports pagination. I'm a god amongst men. I hate maths. - Gui Configs should include "title" key instead of using messages.json now

--- src/main/kotlin/dev/roanoke/rib/gui/configurable/ConfiguredGUI.kt
+++ src/main/kotlin/dev/roanoke/rib/gui/configurable/ConfiguredGUI.kt
@@ -10,12 +10,14 @@ import kotlinx.serialization.json.*
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.server.network.ServerPlayerEntity
 import java.nio.file.Path
+import kotlin.math.ceil
 
 open class ConfiguredGUI(
     val id: String,
     val guiType: GuiType,
     val guiList: List<String>,
     val keys: Map<String, ItemBuilder>,
+    val title: String = "",
     val paginated: Boolean = false,
     val backKey: String = "<",
     val forwardKey: String = ">",
@@ -48,7 +50,23 @@ open class ConfiguredGUI(
                 itemBuilder?.let { key to it }
             }?.toMap(mutableMapOf()) ?: return null
 
-            return ConfiguredGUI(id, guiType, guiList, keys)
+            val title = json["title"]?.jsonPrimitive?.contentOrNull ?: ""
+            val paginated = json["paginated"]?.jsonPrimitive?.booleanOrNull ?: false
+            val backKey = json["backKey"]?.jsonPrimitive?.contentOrNull ?: "<"
+            val forwardKey = json["forwardKey"]?.jsonPrimitive?.contentOrNull ?: ">"
+            val paginatedElement = json["paginatedElement"]?.jsonPrimitive?.contentOrNull ?: "X"
+
+            return ConfiguredGUI(
+                id = id,
+                guiType = guiType,
+                guiList = guiList,
+                keys = keys,
+                title = title,
+                paginated = paginated,
+                backKey = backKey,
+                forwardKey = forwardKey,
+                paginatedElement = paginatedElement
+            )
         }
     }
 
@@ -60,27 +78,102 @@ open class ConfiguredGUI(
         return GuiElementBuilder.from(item.build())
     }
 
-    fun getGui(player: ServerPlayerEntity, elements: Map<String, List<GuiElementBuilder>>, onClose: (ServerPlayerEntity) -> Unit = {}): SimpleGui {
+    fun getPaginatedElementCount(elements: Map<String, List<GuiElementBuilder>>): Int {
+        return elements[paginatedElement]?.let {
+            return it.size
+        } ?: 0
+    }
+
+    fun getRequiredPages(elementCount: Int): Int {
+        val pages = elementCount.toDouble() / guiList.count {
+            it == paginatedElement
+        }
+        return ceil(pages).toInt()
+    }
+
+    fun getPaginatedElementsPerPage(elements: Map<String, List<GuiElementBuilder>>): Int {
+        return guiList.count {
+            it == paginatedElement
+        }
+    }
+
+    fun getPage(requestedPage: Int, elementCount: Int): Int {
+        val requiredPages = getRequiredPages(elementCount)
+        var realPage = requestedPage
+        if (requestedPage > requiredPages) {
+            realPage = 1
+        }
+        if (requestedPage < 1) {
+            realPage = requiredPages
+        }
+        return realPage
+    }
+
+    fun getGui(player: ServerPlayerEntity,
+               elements: Map<String, List<GuiElementBuilder>>,
+               onClose: (ServerPlayerEntity) -> Unit = {},
+               requestedPage: Int = 1): SimpleGui {
          val gui = object : SimpleGui(GuiType.getScreenHandlerType(guiType), player, false) {
             override fun onClose() {
                 onClose(player)
             }
         }
 
+        gui.title = Rib.Rib.parseText(title)
+
+        val paginatedElementCount = getPaginatedElementCount(elements)
+        val page = getPage(requestedPage, paginatedElementCount)
+
         val elementIndex: MutableMap<String, Int> = mutableMapOf();
-        elements.keys.forEach { elementIndex[it] = 0 }
-        keys.keys.forEach { elementIndex[it] = 0 }
+
+        elements.keys.forEach {
+            if (paginated && paginatedElement == it) {
+                val paginatedIndex = getPaginatedElementsPerPage(elements) * (page - 1)
+                elementIndex[it] = paginatedIndex
+            } else {
+                elementIndex[it] = 0
+            }
+        }
+        keys.keys.forEach {
+            if (paginated && paginatedElement == it) {
+                val paginatedIndex = getPaginatedElementsPerPage(elements) * (page - 1)
+                elementIndex[it] = paginatedIndex
+            } else {
+                elementIndex[it] = 0
+            }
+        }
 
         for ((index, key) in guiList.withIndex()) {
-            if (elements.containsKey(key)) {
-                if (elements[key]!!.size > elementIndex[key]!!) {
-                    gui.setSlot(index, elements[key]!![elementIndex[key]!!])
-                    elementIndex[key] = elementIndex[key]!! + 1
-                } else {
-                    gui.setSlot(index, getGuiElementFromKey(key))
+            when (key) {
+                forwardKey -> {
+                    gui.setSlot(index,
+                        getGuiElementFromKey(forwardKey)
+                            .setCallback { _, _, _ ->
+                                getGui(player, elements, onClose, page+1).open()
+                            }
+                        )
                 }
-            } else {
-                gui.setSlot(index, getGuiElementFromKey(key))
+                backKey -> {
+                    gui.setSlot(index,
+                        getGuiElementFromKey(forwardKey)
+                            .setCallback { _, _, _ ->
+                                getGui(player, elements, onClose, page-1).open()
+                            }
+                        )
+                }
+                else -> {
+                    if (elements.containsKey(key)) {
+
+                        if (elements[key]!!.size > elementIndex[key]!!) {
+                            gui.setSlot(index, elements[key]!![elementIndex[key]!!])
+                            elementIndex[key] = elementIndex[key]!! + 1
+                        } else {
+                            gui.setSlot(index, getGuiElementFromKey(key))
+                        }
+                    } else {
+                        gui.setSlot(index, getGuiElementFromKey(key))
+                    }
+                }
             }
         }
 
